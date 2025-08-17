04561841e07bdcd2427153260ac57c228772e6f9
WilliamAraujo <w67543210@gmail.com>
2025-08-17 10:37:29 -0300

    teste

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,13 @@
 function Navbar() {
   return (
-    <div>
+    <div
+      style={{
+        backgroundColor: 'lightblue',
+        padding: '10px',
+        textAlign: 'center',
+        fontWeight: 'bold',
+      }}
+    >
       <h1>Navbar</h1>
     </div>
   );
